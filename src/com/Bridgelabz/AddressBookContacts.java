16b117891662d1327dd3a03ec608a9888641bcab
pihu2810 <vishaka.kadam01@gmail.com>
2022-03-29 19:35:39 +0530

    add  AddressBook UC1

--- src/com/Bridgelabz/AddressBookContacts.java
+++ src/com/Bridgelabz/AddressBookContacts.java
@@ -1,5 +1,76 @@
 package com.Bridgelabz;
 
-public class AddressBookContacts {
+public class AddressBookContacts
+{
+	 private String FirstName;
+	  private String LastName;
+	  private String Address;
+	  private String City;
+	  private String State;
+	  private long Zip;
+	  private long PhoneNumber;
+	  private String Email;
 
+	
+public String getFirstName() {
+		return FirstName;
+	}
+	public void setFirstName(String firstName) {
+		FirstName = firstName;
+	}
+	public String getLastName() {
+		return LastName;
+	}
+	public void setLastName(String lastName) {
+		LastName = lastName;
+	}
+	public String getAddress() {
+		return Address;
+	}
+	public void setAddress(String address) {
+		Address = address;
+	}
+	public String getCity() {
+		return City;
+	}
+	public void setCity(String city) {
+		City = city;
+	}
+	public String getState() {
+		return State;
+	}
+	public void setState(String state) {
+		State = state;
+	}
+	public long getZip() {
+		return Zip;
+	}
+	public void setZip(long zip) {
+		Zip = zip;
+	}
+	public long getPhoneNumber() {
+		return PhoneNumber;
+	}
+	public void setPhoneNumber(long phoneNumber) {
+		PhoneNumber = phoneNumber;
+	}
+	public String getEmail() {
+		return Email;
+	}
+	public void setEmail(String email) {
+		Email = email;
+	}
+
+	public AddressBookContacts(String firstName, String lastName, String address, String city, String state, long zip,
+			long phoneNumber, String email) {
+		super();
+		FirstName = firstName;
+		LastName = lastName;
+		Address = address;
+		City = city;
+		State = state;
+		Zip = zip;
+		PhoneNumber = phoneNumber;
+		Email = email;
+}
 }
