bc4f13f52fdaa06be1ef48c7dd348dae57d57cf7
pihu2810 <vishaka.kadam01@gmail.com>
2022-04-03 19:53:54 +0530

    add AddressBookUC5

--- src/com/Bridgelabz/IAddressBook.java
+++ src/com/Bridgelabz/IAddressBook.java
@@ -2,9 +2,9 @@ package com.Bridgelabz;
 
 public interface IAddressBook
 {
-	void addContact(String FirstName, String LastName, String Address, String City, long Zip,
-			long PhoneNumber, String Email);
-	void printContact();
-	void editContactDetails(String name, long newPhoneNumber);
-	void deleteContactDetails(String name);
+	void  addContact();
+	void editContact(String name) ;
+	void deleteContact(String name);
+	 void addMultipleContacts();
+	void Address(String city, String state, long zip);
 }
