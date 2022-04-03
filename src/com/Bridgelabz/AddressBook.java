b9d0ffe0eecd651493410ec83e7740f0f8712509
pihu2810 <vishaka.kadam01@gmail.com>
2022-04-03 19:52:46 +0530

    add AddressBookUC5

--- src/com/Bridgelabz/AddressBook.java
+++ src/com/Bridgelabz/AddressBook.java
@@ -1,70 +1,89 @@
 package com.Bridgelabz;
-
+import java.util.Iterator;
+import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
-public class AddressBook implements IAddressBook{
-	Scanner scannerObject = new Scanner(System.in);
-	public List<AddressBookContacts> contactList = new ArrayList<AddressBookContacts>();
-
-	public  void addContact(String FirstName, String LastName, String Address, String City, long Zip,
-			long PhoneNumber, String Email) {
-		AddressBookContacts addressBookContacts = new AddressBookContacts();
-		addressBookContacts.setFirstName(FirstName);
-		addressBookContacts.setLastName(LastName);
-		addressBookContacts.setAddress(Address);
-		addressBookContacts.setCity(City);
-		addressBookContacts.setZip(Zip);
-		addressBookContacts.setPhoneNumber(PhoneNumber);
-		addressBookContacts.setEmail(Email);
-		contactList.add(addressBookContacts);
-
-	}
-
-	public  void printContact() {
-		System.out.println("********Print Contact*******");
-		for (int i = 0; i < contactList.size(); i++) {
-			System.out.println("*****Start of user details in Address book**********");
-			System.out.println("First name:" + contactList.get(i).getFirstName());
-			System.out.println("Lastname:" + contactList.get(i).getLastName());
-			System.out.println("Address:" + contactList.get(i).getAddress());
-			System.out.println("City:" + contactList.get(i).getCity());
-			System.out.println("Zip:" + contactList.get(i).getZip());
-			System.out.println("PhoneNumber:" + contactList.get(i).getPhoneNumber());
-			System.out.println("Email:" + contactList.get(i).getEmail());
-
-			System.out.println("*****end of user details in Address book**********");
-		}
-	}
-
-	public void editContactDetails(String name, long newPhoneNumber) {
+public class AddressBook {
+	public static Scanner scanner = new Scanner(System.in);
+	public ArrayList<AddressBookContacts> addressBook = new ArrayList<>();
+	//public HashMap<Integer, Contact> addressBook = new HashMap<>();
+	public void addContact() {
+		System.out.println("Enter name");
+		String name = scanner.next();
+		System.out.println("Enter city");
+		String city = scanner.next(); 
+		System.out.println("Enter state ");
+		String state = scanner.next();
+		System.out.println("Enter zip");
+		long zip = scanner.nextLong();
+		System.out.println("Enter phone");
+		long phone = scanner.nextLong();
+		System.out.println("Enter email");
+		String email = scanner.next();
 		
-		for (int i = 0; i < contactList.size(); i++) {
-			
-			if (contactList.get(i).getFirstName().equals(name)) 
-			{
-				contactList.get(i).setPhoneNumber(newPhoneNumber);
-				}else
-					System.out.println("invalid name");
-		}
+		addressBook.add(new AddressBookContacts(name, city, state, zip, phone, email));
+		
 	}
-	private int haveContact(String name) {
-		for (int i = 0; i < contactList.size(); i++) 
-			if(contactList.get(i).getFirstName().equals(name)) 
-				return i;
-			return -1;
+	public void editContact(AddressBookContacts person) {
+		System.out.println("What do you wish to edit for: "+person.name+".\n Enter 1 for name \n Enter 2 for"
+				+ " address \n Enter 3 for phone number \n Enter 4 for email ");
+		int choice = scanner.nextInt();
+		switch (choice) {
+		case 1:
+			 System.out.println("enter new name");
+			 person.name = scanner.next();
+			 break;
+		case 2:
+			 System.out.println("enter new city");
+			 String city = scanner.next();
+			 System.out.println("enter new state");
+			 String state = scanner.next();
+			 System.out.println("enter new zip");
+			 long zip = scanner.nextLong();
+			 person.address = new Address(city, state, zip);
+			 break;
+		case 3:
+			 System.out.println("enter new phone");
+			 person.phoneNumber = scanner.nextLong();
+			 break;
+		case 4:
+			 System.out.println("enter new email");
+			 person.email = scanner.next();
+			 break;
 		}
+//		 System.out.println(person);
+	}
 	
-	public void deleteContactDetails(String name) 
-	{
-		int place=haveContact(name);
-		if(place>=0) {
-			contactList.remove(place);
-			
+	public void editContact(String name) {
+		Iterator<AddressBookContacts> iterator = addressBook.iterator();
+		while(iterator.hasNext()) {
+	
+			AddressBookContacts person = iterator.next();
+		    if(name.equalsIgnoreCase(person.name)) {
+		    	editContact(person);	
+		    }
 		}
-		
+	}
+	
+	public void deleteContact(String name) {
+		     
+		    boolean isDeleted = addressBook.removeIf(contact -> (contact.name.equalsIgnoreCase(name)) );
+		    if(isDeleted)
+		    	System.out.println(name+"'s contact deleted successfully!!!");
+		    else {
+		    	System.out.println("NO SUCH CONTACT EXITS!!! PLEASE ENTER VALID NAME");
+		        deleteContact(scanner.next());
+		    }
 	}
 
+	public void addMultipleContacts() {
+		System.out.println("How many person you want ro add?");
+		int numberOfContacts = scanner.nextInt();
+		for (int i=0;i<numberOfContacts;i++)
+			this.addContact();
+	}
+	
+	
 	}
 
