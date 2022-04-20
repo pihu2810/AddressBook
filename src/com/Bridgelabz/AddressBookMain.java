5defffdf9d36e2f3ad3e6d24898cf27463389585
pihu2810 <vishaka.kadam01@gmail.com>
2022-04-20 15:02:38 +0530

    Refactored AddressBook to avoid duplicate persons

--- src/com/Bridgelabz/AddressBookMain.java
+++ src/com/Bridgelabz/AddressBookMain.java
@@ -1,27 +1,34 @@
 package com.Bridgelabz;
 
+import java.util.Map;
 import java.util.Scanner;
 
 public class AddressBookMain {
-	
-	private static Scanner scanner;
-
 	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
 		System.out.println("Welcome To Address Book Program!!!!!");
-		AddressBook iaddressBook = new AddressBook();
-		iaddressBook.addContact();
+		IAddressBook addressBook=new AddressBook();
+		
+		addressBook.addContact();
 
 		System.out.println("Enter name of person you wish to edit");
-		String name = scanner.next();
-		iaddressBook.editContact(name);
+		String name = sc.next();
+		addressBook.editContact(name);
 
 		System.out.println("Enter name of person you wish to delete");
-		String deletePerson = scanner.next();
-		iaddressBook.deleteContact(deletePerson);
-		System.out.println("Address Book contains: \n" + iaddressBook.addressBook);
+		String deletePerson = sc.next();
+		addressBook.deleteContact(deletePerson);
+		System.out.println("Address Book contains: \n" + addressBook);
 
-		iaddressBook.addMultipleContacts();
-		System.out.println(iaddressBook.addressBook);
+		addressBook.addMultipleContacts();
+		System.out.println(addressBook);
+
+		AddressBookDictionary addressBookDictionary = new AddressBookDictionary();
+		addressBookDictionary.addMultipleAddressBook();
+		for(Map.Entry<String, AddressBook> e: addressBookDictionary.dictionaryOfAddressBooks.entrySet())
+		{
+			System.out.println(e.getKey()+" " +e.getValue());
+		}
 	}
-
 }
+
