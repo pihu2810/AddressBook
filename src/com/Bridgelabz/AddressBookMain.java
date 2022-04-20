3f18f4e271e3ccd36f2edfdba44e29c3fbb2e88f
pihu2810 <vishaka.kadam01@gmail.com>
2022-04-20 18:27:00 +0530

    Refactored AddressBookDictionary to Sort person Detials by City State or Zip

--- src/com/Bridgelabz/AddressBookMain.java
+++ src/com/Bridgelabz/AddressBookMain.java
@@ -5,35 +5,24 @@ import java.util.Scanner;
 
 public class AddressBookMain {
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		System.out.println("Welcome To Address Book Program!!!!!");
-		IAddressBook addressBook=new AddressBook();
-		
-		addressBook.addContact();
-
-		System.out.println("Enter name of person you wish to edit");
-		String name = sc.next();
-		addressBook.editContact(name);
-
-		System.out.println("Enter name of person you wish to delete");
-		String deletePerson = sc.next();
-		addressBook.deleteContact(deletePerson);
-		System.out.println("Address Book contains: \n" + addressBook);
-		addressBook.addMultipleContacts();
-		System.out.println(addressBook);
-
+	
+   Scanner sc=new Scanner(System.in);
 		AddressBookDictionary addressBookDictionary = new AddressBookDictionary();
 		addressBookDictionary.addMultipleAddressBook();
 		addressBookDictionary.printAddressBookDictionary();
-		System.out.println("enter the place to be search");
-		String place=sc.next();
-		addressBookDictionary.searchPersonInCityOrState(place);
-		System.out.println("enter the place to be view");
-		String place1=sc.next();
-		addressBookDictionary.viewPersonByCityOrState(place1);
-		System.out.println("enter the place to get the person count");
-		String place3=sc.next();
-		addressBookDictionary.getPersonsCountByCityOrState(place3);
+//	System.out.println("enter the place to be search");
+//		String place=sc.next();
+//		addressBookDictionary.searchPersonInCityOrState(place);
+//		System.out.println("enter the place to be view");
+//		String place1=sc.next();
+//		addressBookDictionary.viewPersonByCityOrState(place1);
+//		System.out.println("enter the place to get the person count");
+//		String place3=sc.next();
+//		addressBookDictionary.getPersonsCountByCityOrState(place3);
+		addressBookDictionary.sortPersonByNames();
+	    addressBookDictionary.sortPersonByCity();
+		addressBookDictionary.sortPersonByState();
+		addressBookDictionary.sortPersonByZip();
 		
 		
 	}
