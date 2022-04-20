6f6122278159b20a22ecbbc7b2eb4318ca3ff5a2
pihu2810 <vishaka.kadam01@gmail.com>
2022-04-20 15:02:01 +0530

    Refactored AddressBook to avoid duplicate persons

--- src/com/Bridgelabz/AddressBookDictionary.java
+++ src/com/Bridgelabz/AddressBookDictionary.java
@@ -1,22 +1,14 @@
 package com.Bridgelabz;
 import java.util.HashMap;
-import java.util.Map;
+
 
 public class AddressBookDictionary 
 {
 	HashMap<String, AddressBook> dictionaryOfAddressBooks = new HashMap<>();
     static int count=0;
-    public static void main(String[] args) {
-        AddressBookDictionary addressBookDictionary = new AddressBookDictionary();
-        addressBookDictionary.addMultipleAddressBook();
-        //System.out.println(addressBookDictionary);
-        for(Map.Entry<String, AddressBook> e: addressBookDictionary.dictionaryOfAddressBooks.entrySet()){
-            System.out.println(e.getKey()+" " +e.getValue());
-        }
-    }
 
-    private void addMultipleAddressBook() {
-        System.out.println("How many AddressBooks you want to add?");
+    public void addMultipleAddressBook() {
+        System.out.println("How many AddressBooks do you want to create?");
         int numberOfAddressBooks = AddressBook.scanner.nextInt();
         for (int i = 0;i < numberOfAddressBooks; i++) {
             count++;
